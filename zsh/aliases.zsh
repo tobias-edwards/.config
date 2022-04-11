9b758df605324b9d1e5ce41c0de7f4c7353625f7
tobias-edwards <tobiasrichardedwards@gmail.com>
2022-04-11 15:34:58 +0100

    feat: View .msg files

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,9 @@
 alias v="nvim"
 alias vi="nvim"
 
-# Temp
+# Open GUI to view .msg files
+alias msgviewer="cd ~/Repos/MsgViewer/MSGViewer/target/ && java -jar msgviewer.jar"
+
+# Quickly source zshrc
 alias s="source ~/.zshrc"
+
