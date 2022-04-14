4bc92066d99acd62e942d97820cb74ef89f29100
tobias-edwards <tobiasrichardedwards@gmail.com>
2022-04-14 14:12:18 +0100

    feat(telescope): Include hidden files in pickers

--- nvim/lua/core/telescope.lua
+++ nvim/lua/core/telescope.lua
@@ -90,13 +90,17 @@ local setup = {
 		},
 	},
 	pickers = {
-		-- Default configuration for builtin pickers goes here:
-		-- picker_name = {
-		--   picker_config_key = value,
-		--   ...
-		-- }
-		-- Now the picker_config_key will be applied every time you call this
-		-- builtin picker
+		find_files = {
+			hidden = true,
+		},
+		live_grep = {
+			additional_args = function(opts)
+				local additional_args = {
+					"--hidden",
+				}
+				return vim.tbl_deep_extend("force", additional_args, opts)
+			end,
+		},
 	},
 	extensions = {
 		-- Native sorter to improve performance
